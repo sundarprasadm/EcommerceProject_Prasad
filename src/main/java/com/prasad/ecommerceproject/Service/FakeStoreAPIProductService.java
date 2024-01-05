a57c834dcb090841890ef7ab2ca005112ab1b772
SUNDARPRASAD M <sundarprasad22@gmail.com>
2024-01-06 00:02:30 +0530

    added post and put method

--- src/main/java/com/prasad/ecommerceproject/Service/FakeStoreAPIProductService.java
+++ src/main/java/com/prasad/ecommerceproject/Service/FakeStoreAPIProductService.java
@@ -4,8 +4,14 @@ import com.prasad.ecommerceproject.Models.Category;
 import com.prasad.ecommerceproject.Models.Product;
 import com.prasad.ecommerceproject.dto.FakeStoreProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpMessageConverterExtractor;
+import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class FakeStoreAPIProductService implements ProductService {
@@ -26,6 +32,7 @@ public class FakeStoreAPIProductService implements ProductService {
         product.setImageUrl(fakeStoreProduct.getImage());
         product.setCategory(new Category());
         product.getCategory().setName(fakeStoreProduct.getCategory());
+        product.getCategory().setId(fakeStoreProduct.getId());
 
         return product;
     }
@@ -39,5 +46,71 @@ public class FakeStoreAPIProductService implements ProductService {
         return convertFakeStoreProductToProduct(productDto);
     }
 
+    @Override
+    public List<Product> getAllProducts(){
+        //        List<FakeStoreProductDto> response = restTemplate.getForObject(
+//                "https://fakestoreapi.com/products",
+//                List<FakeStoreProductDto>.class
+//        );
+        // runtime
+        FakeStoreProductDto[] response = restTemplate.getForObject(
+                "https://fakestoreapi.com/products",
+                FakeStoreProductDto[].class
+        );
+        List<Product> answer = new ArrayList<>();
+        if(response!=null){
+            for (FakeStoreProductDto dto: response) {
+                answer.add(convertFakeStoreProductToProduct(dto));
+            }
+        }
 
+
+        return answer;
+    }
+
+    @Override
+    public Product createNewProduct(Product product) {
+
+        FakeStoreProductDto fakeStoreProductDto = new FakeStoreProductDto();
+        fakeStoreProductDto.setTitle(product.getTitle());
+        fakeStoreProductDto.setPrice(product.getPrice());
+        fakeStoreProductDto.setDescription(product.getDescription());
+        if(null!=product.getImageUrl()){
+            fakeStoreProductDto.setImage(product.getImageUrl());
+        }
+        FakeStoreProductDto fakeStoreProduct = restTemplate.postForObject(
+                "https://fakestoreapi.com/products",fakeStoreProductDto,
+                FakeStoreProductDto.class);
+        if(fakeStoreProduct!=null){
+            return convertFakeStoreProductToProduct(fakeStoreProduct);
+        }
+        return new Product();
+
+    }
+
+    @Override
+    public Product updatePartialProduct(Long id, Product product) {
+        return null;
+    }
+
+    @Override
+    public Product replaceproduct(Long id, Product product){
+        FakeStoreProductDto fakeStoreProductDto = new FakeStoreProductDto();
+        fakeStoreProductDto.setTitle(product.getTitle());
+        fakeStoreProductDto.setPrice(product.getPrice());
+        fakeStoreProductDto.setDescription(product.getDescription());
+        fakeStoreProductDto.setImage(product.getImageUrl());
+        fakeStoreProductDto.setCategory(product.getCategory().getName());
+        RequestCallback requestCallback = restTemplate.httpEntityCallback(fakeStoreProductDto, FakeStoreProductDto.class);
+        HttpMessageConverterExtractor<FakeStoreProductDto> responseExtractor =
+                new HttpMessageConverterExtractor<>(FakeStoreProductDto.class, restTemplate.getMessageConverters());
+        FakeStoreProductDto response = restTemplate.execute("https://fakestoreapi.com/products/" + id, HttpMethod.PUT, requestCallback, responseExtractor);
+
+        return convertFakeStoreProductToProduct(response);
+    }
+
+    @Override
+    public void deleteProduct(Long id){
+
+    }
 }
--- src/main/java/com/prasad/ecommerceproject/Service/ProductService.java
+++ src/main/java/com/prasad/ecommerceproject/Service/ProductService.java
@@ -3,9 +3,23 @@ package com.prasad.ecommerceproject.Service;
 import com.prasad.ecommerceproject.Models.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface ProductService {
 
     Product getSingleProduct(Long id);
+
+    List<Product> getAllProducts();
+
+    //Create new Product
+    Product createNewProduct(Product product);
+
+    Product replaceproduct(Long id,Product product);
+    Product updatePartialProduct(Long id,Product product);
+
+    void deleteProduct(Long id);
+
+
 
 }
